6ad65d2780e1d82ae5eeeba64fa089f0903912df
IoannTrubachev <ivan.trubachev@gmail.com>
2017-03-30 22:20:57 -0400

    First Commit

---
+++ LinkHubUI/LinkHubUI/Areas/Admin/Controllers/LsitCategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.Admin.Controllers
+{
+    public class LsitCategoryController : Controller
+    {
+        // GET: Admin/LsitCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
